c2195189f362f340d91d4ff3035286c4d0d7b530
Moobbot <ngotam24082001@gmail.com>
2023-03-09 14:11:48 +0700

    update 2

--- AND_GATE/perceptron_ANDgate.py
+++ AND_GATE/perceptron_ANDgate.py
@@ -52,29 +52,41 @@ class Perceptron:
 # Mỗi lần lặp lại sẽ duyệt qua từng đầu vào và cập nhật trọng số cho mô hình.
 
     def train(self, training_inputs, labels, n_iterations):
-        for i in range(n_iterations):
+        count = 0
+        error = 0.1
+        while count < n_iterations and error != 0:
+            error = 0
+            print("Round " + str(count+1) + ":")
             # So sánh giá trị đầu ra tính được với giá trị thực tế
             # Sử dụng hàm zip để lặp qua hai mảng cùng lúc.
             for inputs, label in zip(training_inputs, labels):
                 prediction = self.predict(inputs)
-                # sai số error = lấy hiệu giữa nhãn và giá trị dự đoán.
-                error = label - prediction
-
-                self.weights += self.learning_rate * error * inputs  # Cập nhật trọng số
-                self.bias += self.learning_rate * error  # Cập nhật bias
-                # print('weights = ', self.weights)
-                # print('bias = ', self.bias)
-                # print("+------+-------+")
+                if (prediction != label):
+                    # sai số error = lấy hiệu giữa nhãn và giá trị dự đoán.
+                    error = label - prediction
+                    self.weights += self.learning_rate * error * inputs  # Cập nhật trọng số
+                    self.bias += self.learning_rate * error  # Cập nhật bias
+                    print("d = " + str(label)+"; y = " +
+                          str(prediction) + " => e = "+str(error))
+                count += 1
+        print('weights = ', self.weights)
+        print('bias = ', self.bias)
+        print("+------+-------+")
 
 
 # Hàm kiểm tra đưa ra kết quả của bảng giá trị chân lý
 
 
 def has_zero(s):
+
     for c in s:
         if c == 0:
             return 0
     return 1
+
+
+def has_zero2(s):
+    return int(all(s))
 
 # Hàm tạo các chuỗi nhị phân kích thước m
 
@@ -87,15 +99,18 @@ def generate_binary_strings(N):
     return binary_strings
 
 
-# Example usage
+def generate_binary_strings2(m):
+    return [list(map(int, format(i, '0{}b'.format(m)))) for i in range(2**m)]
+
+    # Example usage
 m = int(input("Số lượng đầu vào: "))
 # m = 3
 # Tạo ma trận chứa các trường hợp trong bảng giá trị chân lý
-training_inputs = np.array(generate_binary_strings(m))
+training_inputs = np.array(generate_binary_strings2(m))
 labels = []
 # kết quả của từng trường hợp 1
 for item in training_inputs:
-    labels.append(has_zero(item))
+    labels.append(has_zero2(item))
 
 print("+---------------+----------------+")
 print(" | AND Truth Table | Result |")
